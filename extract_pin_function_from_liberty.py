bb4f3e2d52adab634b4fc1b5a00f117be40296ee
hrshishym <chappy.dogheart@gmail.com>
2020-09-13 19:31:17 +0900

    Update extract_pin_function_from_liberty.py
    * Add statetable output
    * Add setting for selecting attributes to be output
    * Add string replace for multiline statetable
    * Add debug switch

--- extract_pin_function_from_liberty.py
+++ extract_pin_function_from_liberty.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 
+### Setting 
+cell_attributes = ["clock_gating_integrated_cell"]
+ff_attributes   = []
+pin_attributes  = ["direction", "clock", "function", "state_function"]
+
+import os
+import sys
+import re
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'modules'))
+
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input")
+parser.add_argument("-d", "--debug", action="store_true")
 args = parser.parse_args()
 
 from liberty.parser import parse_liberty
 
-tlib = parse_liberty(open(args.input).read())
+tdata = re.sub("\\\\\n", "", open(args.input).read())
+tlib = parse_liberty(tdata)
+
+if args.debug:
+    import pprint
+    pprint.pprint(tlib)
 
 # library (sxlib013) {
 print("library ({}) {{".format(tlib.args[0]))
@@ -20,7 +37,25 @@ for eachlib in tlib.groups:
     # cell(a2_x2) { /* 2008-01-10:21h05 */
     print("cell({}) {{".format(cellname))
 
+    if args.debug:
+        print("==")
+        pprint.pprint(eachlib)
+        print("==")
+        pprint.pprint(eachlib.attributes)
+        print("==")
+
+    ### Print cell attributes
+    for eachattr in eachlib.attributes:
+        for eachattr in cell_attributes:
+            if eachattr in eachlib.attributes.keys():
+                print("  {} : {} ;".format(eachattr, eachlib.attributes[eachattr]))
+
+    ### Print sub group
     for eachgroup in eachlib.groups:
+        if args.debug:
+            print("====")
+            pprint.pprint(eachgroup)
+            print("====")
         if eachgroup.group_name == "ff":
             # ff
             print("  ff({}) {{ ".format(",".join(eachgroup.args)))
@@ -30,12 +65,18 @@ for eachlib in tlib.groups:
         elif eachgroup.group_name == "pin":
             ## pin
             print("  pin({}) {{".format(eachgroup.args[0]))
-            if "direction" in eachgroup.attributes.keys():
-                print("    {} : {} ;".format("direction", eachgroup.attributes["direction"]))
-            if "clock" in eachgroup.attributes.keys():
-                print("    {} : {} ;".format("clock", eachgroup.attributes["clock"]))
-            if "function" in eachgroup.attributes.keys():
-                print("    {} : {} ;".format("function", eachgroup.attributes["function"]))
+            for eachattr in pin_attributes:
+                if eachattr in eachgroup.attributes.keys():
+                    print("    {} : {} ;".format(eachattr, eachgroup.attributes[eachattr]))
+            print("  }")
+        elif eachgroup.group_name == "statetable":
+            ## statetable
+            tarr = []
+            for i in eachgroup.args:
+                tarr.append(str(i))
+            print("  statetable( {} ) {{".format(" , ".join(tarr)))
+            if "table" in eachgroup.attributes.keys():
+                print("    {} : {} ;".format("table", re.sub(",", ", \\\n", str(eachgroup.attributes["table"]))))
             print("  }")
     print("}")
 print("}")
